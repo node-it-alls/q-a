175ae9e31065d1f205ce34fcf866db6aec192e57
Skyla Reiner <sysreiner@gmail.com>
2024-11-04 21:23:12 -0800

    Add API endpoints for QA functionality and update package scripts

---
+++ controllers.js
@@ -0,0 +1,129 @@
+const { Answer, Question } = require("./db.js");
+
+exports.getQuestions = (req, res) => {
+  const page = req.query.page || 1;
+  const count = req.query.count || 5;
+  const skip = (page - 1) * count;
+  Question.find({ product_id: req.query.product_id, reported: false })
+    .skip(skip)
+    .limit(count)
+    .exec()
+    .then((doc) => {
+      const results = Promise.all(
+        doc.map(async (question) => {
+          const answers = await Answer.find({ question_id: question.id });
+          const mapped = {};
+          if (answers) {
+            answers.forEach((answer) => {
+              mapped[String(answer.id)] = {
+                ...answer._doc,
+                answer_id: answer.id,
+              };
+            });
+          }
+          return { ...question._doc, answers: mapped };
+        })
+      );
+      return results;
+    })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.createQuestion = (req, res) => {
+  const { body, name, email, product_id } = req.query;
+  if (!body || !name || !email || !product_id) {
+    res.sendStatus(400);
+    return;
+  }
+  Question.countDocuments({}).then((count) => {
+    const newQuestion = new Question({
+      question_body: body,
+      asker_name: name,
+      question_helpfulness: 0,
+      product_id,
+      id: count + 1,
+    });
+    newQuestion
+      .save()
+      .then((doc) => {
+        res.status(201).send(doc);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+};
+
+exports.reportQuestion = (req, res) => {
+  Question.findOneAndUpdate({ id: req.params.question_id }, { reported: true })
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.createAnswer = (req, res) => {
+  const { body, name, email, photos } = req.query;
+  if (!body || !name || !email || !photos) {
+    res.sendStatus(400);
+    return;
+  }
+  Answer.countDocuments({}).then((count) => {
+    const newAnswer = new Answer({
+      body,
+      answerer_name: name,
+      photos: JSON.parse(req.query.photos),
+      id: count + 1,
+      question_id: req.params.question_id,
+    });
+    newAnswer
+      .save()
+      .then(() => {
+        res.sendStatus(201);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+};
+
+exports.updateQuestionHelpful = (req, res) => {
+  Question.findOneAndUpdate(
+    { id: req.params.question_id },
+    { $inc: { question_helpfulness: 1 } }
+  )
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.reportAnswer = (req, res) => {
+  Answer.findOneAndUpdate({ id: req.params.answer_id }, { reported: true })
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.updateAnswerHelpful = (req, res) => {
+  Answer.findOneAndUpdate(
+    { id: req.params.answer_id },
+    { $inc: { helpfulness: 1 } }
+  )
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
--- index.js
+++ index.js
@@ -1,21 +1,45 @@
-const express = require('express');
-const morgan = require('morgan');
-const db = require('./db.js');
+const express = require("express");
+const morgan = require("morgan");
+const db = require("./db.js");
 const app = express();
 const PORT = process.env.PORT || 3003;
+const controllers = require("./controllers.js");
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('hi from home')
-})
-
-app.get('/qa', (req, res) => {
-  // console.error('Error:', err.stack);
-  res.send('Hello! QA!')
+app.get("/qa/questions", (req, res) => {
+  controllers.getQuestions(req, res);
 });
 
+app.get("/qa", (req, res) => {
+  res.send("Hello! QA!");
+});
+
+app.post("/qa/questions", (req, res) => {
+  controllers.createQuestion(req, res);
+});
+
+app.post("/qa/questions/:question_id/answers", (req, res) => {
+  controllers.createAnswer(req, res);
+});
+
+app.put("/qa/questions/:question_id/report", (req, res) => {
+  console.log("reported question", req.params);
+  controllers.reportQuestion(req, res);
+});
+
+app.put("/qa/questions/:question_id/helpful", (req, res) => {
+  controllers.updateQuestionHelpful(req, res);
+});
+
+app.put("/qa/answers/:answer_id/report", (req, res) => {
+  controllers.reportAnswer(req, res);
+});
+
+app.put("/qa/answers/:answer_id/helpful", (req, res) => {
+  controllers.updateAnswerHelpful(req, res);
+});
 
 app.listen(PORT, () => {
   console.log(`Data server running on http://localhost:${PORT}`);
